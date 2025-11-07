0eb19cc50d7804580a6752e9d599e22d05455de7
linikers <linikers@hotmail.com>
2025-11-07 17:12:12 -0300

    enviroment

---
+++ scripts/enviroment.ts
@@ -0,0 +1,4 @@
+// TELEGRAM_BOT_TOKEN=8254769065:AAHLPzG9ooNOWCCAo0_QYb9UB-wshTMr0hU
+// WEBHOOK_URL=https://telebot-bay-chi.vercel.app/api/webhook
+// #WEBHOOK_URL=https://telebot-mrtf18964-linikers-projects.vercel.app/api/webhook
+//modelo env
